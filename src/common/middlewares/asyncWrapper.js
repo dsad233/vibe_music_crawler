0af16672c9d94345b2bd69dea78838e4ef8039f2
dsad233 <ksl1421@naver.com>
2025-10-22 21:29:50 +0900

    fix: wrapper 수정

--- src/common/middlewares/asyncWrapper.js
+++ src/common/middlewares/asyncWrapper.js
@@ -1,5 +1,5 @@
 export const asyncWrapper = (callback) => {
-  return (req, res, next) => {
-    callback(req, res, next).catch(next);
+  return async (req, res, next) => {
+    await callback(req, res, next).catch(next);
   };
 };
